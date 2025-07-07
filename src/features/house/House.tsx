d7d8155bdb546eeca5d818de710b9ba16099a547
Arunabh <96439469+Arunabh-P@users.noreply.github.com>
2025-07-07 21:18:33 +0530

    third commit

--- src/features/house/House.tsx
+++ src/features/house/House.tsx
@@ -6,8 +6,14 @@ const House = () => {
     if (isLoading) return <p>Loading...</p>;
     if (error) return <p>Error fetching houses</p>;
     return (
-        <div>{data?.data?.houses?.map((house) => (
-            <div key={house._id}>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-col-3 lg:grid-cols-4 gap-8'>{data?.data?.houses?.map((house) => (
+            <div key={house._id} className='w-[300px]'>
+                <div style={{
+                    backgroundImage: `url(${house.images[0]})`,
+                }}
+                    className='bg-cover bg-center h-[300px] w-[300px] rounded-lg'
+                >
+                </div>
                 <h3 className='text-red-600'>{house.title}</h3>
                 <p>{house.location} - ₹{house.price}</p>
                 <p>{house.propertyType} and {house.Furnishing}</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,21 +7,23 @@ const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
   const addHouseRef = useRef<any>(null);
   const handleClose = () => {
-      addHouseRef.current?.resetForm?.();
+    addHouseRef.current?.resetForm?.();
     setIsOpen(false);
   };
   return (
-    <div>
-       <button
-        onClick={() => setIsOpen(true)}
-        className="border-2 border-black rounded-full text-black px-4 py-2 text-[16px] md:text-[18px] font-normal hover:bg-black hover:text-white cursor-pointer"
-      >
-        ADD NEW HOUSE DATA
-      </button>
+    <div className='px-5 md:px-12 lg:px-16 flex flex-col items-center'>
+      <div className='w-full flex justify-end py-5'>
+        <button
+          onClick={() => setIsOpen(true)}
+          className="border-2 border-black rounded-full text-black px-4 py-2 text-[14px] md:text-[16px] font-normal hover:bg-black hover:text-white cursor-pointer"
+        >
+          ADD NEW HOUSE DATA
+        </button>
+      </div>
       <Drawer position="bottom" isOpen={isOpen} onClose={handleClose}>
-        <AddHouse ref={addHouseRef}/>
+        <AddHouse ref={addHouseRef} />
       </Drawer>
-        <House/>
+      <House />
     </div>
   )
 }
