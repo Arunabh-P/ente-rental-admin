30468b8db30a1ab58ec5c1e3944a926dc2c4d709
Arunabh <96439469+Arunabh-P@users.noreply.github.com>
2025-07-13 22:04:47 +0530

    lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import SingleHouse from "./pages/single-house"
-import AllHouses from "./pages/all-houses"
-import Dashboard from "./pages/dashboard"
-import NotFound from "./components/not-found"
+import { lazy, Suspense } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+const SingleHouse = lazy(() => import("./pages/single-house"));
+const AllHouses = lazy(() => import("./pages/all-houses"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const NotFound = lazy(() => import("./components/not-found"));
 
 function App() {
-
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/houses" element={<AllHouses />} />
-        <Route path="/houses/:slug" element={<SingleHouse />} />
-         <Route path="*" element={<NotFound text="Page not found" statusCode={404} backTo="/" />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/houses" element={<AllHouses />} />
+          <Route path="/houses/:slug" element={<SingleHouse />} />
+          <Route
+            path="*"
+            element={
+              <NotFound text="Page not found" statusCode={404} backTo="/" />
+            }
+          />
+        </Routes>
+      </Suspense>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
